deddc0cf7cbbf2444d0926d6d93a6e5589981135
Eric Mencarini <ericmencarini@outlook.com>
2025-09-25 21:12:38 -0300

    add_1_hard

--- 03.hard/Senior_Managers.sql
+++ 03.hard/Senior_Managers.sql
@@ -1,2 +1,13 @@
 --https://datalemur.com/questions/senior-managers-reportees
 
+SELECT 
+  ee.manager_name,
+  COUNT(DISTINCT ee.emp_id) AS direct_reportees
+FROM
+  employees e 
+  JOIN employees ee ON e.manager_id = ee.emp_id
+    JOIN employees eee ON ee.manager_id = eee.emp_id
+GROUP BY
+  ee.manager_name
+ORDER BY 
+  direct_reportees DESC
